aca57072107c23e719d51b873582fa7dd69c7a2d
Stanciu Dragoș Ioan <39739297+StanciuDragosIoan@users.noreply.github.com>
2020-03-05 17:03:10 +0200

    Commit 2

--- api_masterclass/notes_init.js
+++ api_masterclass/notes_init.js
@@ -1,4 +1,24 @@
 /*
 
     covered till video: 4
+
+    covered till video:
+
+
+    HTTP and the Node Http module
+
+        is the protocol of the web (communicates between clt and server)
+
+        the request-response-cycle:
+        
+            server takes request from client (client sends data)
+                with the request we can update DB/authenticate user and then send back a response + a status code
+
+            each request and response has a header and a body
+
+                the header is a set of key-value pairs (like content-type of the served resource, content length, auth token)
+
+                the body contains data sent to the server or received from it
+
+                use the raw http module (not express)
 */
