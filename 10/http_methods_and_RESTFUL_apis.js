56104947d9d942f27d9ef95b6196b6076fa08f78
Stanciu Dragoș Ioan <39739297+StanciuDragosIoan@users.noreply.github.com>
2020-03-27 12:28:11 +0200

    finished video 10

---
+++ 10/http_methods_and_RESTFUL_apis.js
@@ -0,0 +1,27 @@
+/*
+
+    GET = Retrieve resource
+
+    POST = Submit/Create Resource
+
+    PUT/PATCH = Update Resource
+
+    DELETE = Destroy/Delete Resource
+
+
+
+    API Standard URLs
+
+    GET/todos -fetch all todos
+
+    GET/todos/1 -fetch todo with id 1
+
+    POST/todos - post a new todo
+
+    PUT/todos/1 - update todo with id 1
+
+    DELETE/todos/1 - delete todo with id 1
+
+
+
+*/
